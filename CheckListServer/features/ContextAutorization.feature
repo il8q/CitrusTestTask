fb65a75978e4347d376d34305bbfe4cbda5d92c9
il8q <ilarm8q@gmail.com>
2022-01-14 16:02:43 +0300

    Добавлен каркас тестов для контекста авторизации

---
+++ CheckListServer/features/ContextAutorization.feature
@@ -0,0 +1,16 @@
+Feature: Autorization context
+  Container "Чек-лист сервер" => Component "Ядро бизнес-логики"
+	
+  Scenario: Guest can register and become a user
+    Given e-mail "guestTest@gmail.com" and password "passwordTest1"
+    When e-mail and password not found in User list
+    Then create User with e-mail and password
+    And add to User list
+    And guest transfer to "Start page"
+
+  Scenario: Guest can autorizate and transfer to "Main page"
+    Given e-mail "guestTest@gmail.com" and password "passwordTest1"
+    When e-mail found
+    And password equal password from User list
+    Then guest stand User
+    And User transfer to "Start page"
---
+++ CheckListServer/features/bootstrap/ContextAutorization.php
@@ -0,0 +1,85 @@
+<?php
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use PHPUnit\Framework\Assert as Assert;
+
+class ContextAutorization implements SnippetAcceptingContext
+{
+    public function __construct()
+    {
+    }
+    
+    /**
+     * @Given e-mail :arg1 and password :arg2
+     */
+    public function eMailAndPassword($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When e-mail and password not found in User list
+     */
+    public function eMailAndPasswordNotFoundInUserList()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then create User with e-mail and password
+     */
+    public function createUserWithEMailAndPassword()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then add to User list
+     */
+    public function addToUserList()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then guest transfer to :arg1
+     */
+    public function guestTransferTo($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When e-mail found
+     */
+    public function eMailFound()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When password equal password from User list
+     */
+    public function passwordEqualPasswordFromUserList()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then guest stand User
+     */
+    public function guestStandUser()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then User transfer to :arg1
+     */
+    public function userTransferTo($arg1)
+    {
+        throw new PendingException();
+    }
+}
