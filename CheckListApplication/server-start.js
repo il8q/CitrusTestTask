4fc8460d8d5e4005fa8c3df552d4157c8074dfd7
il8q <ilarm8q@gmail.com>
2022-01-22 14:13:34 +0300

    Добавлен интерфейс стартовой страницы

--- CheckListApplication/server-start.js
+++ CheckListApplication/server-start.js
@@ -6,10 +6,13 @@ const twig = require("twig");
 const app = express()
 const port = 3000;
 
+const path = require('path');
+
 app.set("twig options", {
     allow_async: true, // Allow asynchronous compiling
     strict_variables: false
 });
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('pageContent\\startPage.twig', {
