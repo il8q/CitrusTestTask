02a4baeb3a12de297440b8d5c3c874ec6a46b8da
il8q <ilarm8q@gmail.com>
2022-01-25 22:14:47 +0300

    Переделан дизайн страницы регистрации

--- CheckListApplication/src/views/Register.vue
+++ CheckListApplication/src/views/Register.vue
@@ -2,14 +2,30 @@
   <div class="register">
     <div>
       <form @submit.prevent="submit">
-        <div>
-          <label for="email">Email:</label>
-          <input type="text" name="email" v-model="form.email" />
+        <div class="field-container to-center m-tb-5">
+          <div class="field-name inline-block on-left-side m-r-0" for="email">
+              Email:
+            </div>
+            <div class="inline-block on-right-side">
+              <input 
+                type="text" 
+                name="email"
+                v-model="form.email" 
+               />
+            </div>
         </div>
-        <div>
-          <label for="password">Password:</label>
-          <input type="password" name="password" v-model="form.password" />
-        </div>
+        <div class="field-container to-center m-tb-5">
+           <div class="field-name inline-block on-left-side m-r-0" for="password">
+              Password:
+            </div>
+            <div class="inline-block on-right-side">
+              <input 
+                 type="password" 
+                 name="password"
+                 v-model="form.password"
+               />
+            </div>
+          </div>
         <button type="submit">Submit</button>
       </form>
     </div>
@@ -52,28 +68,64 @@ export default {
   box-sizing: border-box;
 }
 
-label {
+.field-container {
+  width: 400px;
+}
+
+.field-name {
   padding: 12px 12px 12px 0;
-  display: inline-block;
+  width: 20%;
 }
 
 button[type="submit"] {
-  background-color: #4caf50;
-  color: white;
-  padding: 12px 20px;
+  background-color: #bfbfbf;
+  color: #0e0e0e;
+  padding: 6px 20px;
   cursor: pointer;
   border-radius: 30px;
+  font-weight : bold ;
 }
 
 button[type="submit"]:hover {
   background-color: #45a049;
 }
 
+.on-left-side {
+  text-align: left;
+}
+
+.on-right-side {
+  text-align: right;
+}
+
+.to-center {
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.m-lr-5 {
+  margin-left: 5px;
+  margin-right: 5px;
+}
+
+.m-tb-5 {
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+
+.m-r-0 {
+  margin-right: 0px;
+}
+
 input {
   margin: 5px;
   box-shadow: 0 0 15px 4px rgba(0, 0, 0, 0.06);
   padding: 10px;
-  border-radius: 30px;
+  border-radius: 5px;
+}
+
+.inline-block{
+  display: inline-block;
 }
 #error {
   color: red;
